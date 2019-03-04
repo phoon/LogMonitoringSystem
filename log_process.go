a4169ca712055270abea2048d3be1a6478ba31c6
Peven <iampeven@gmail.com>
2019-03-04 15:57:34 +0800

    lesson 2-3

--- log_process.go
+++ log_process.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"sync"
+	"time"
 )
 
 //使用接口机制，增强程序可扩展性
 type Reader interface {
-	Read(rc chan string)
+	Read(rc chan []byte)
 }
 
 type Writer interface {
@@ -16,7 +20,7 @@ type Writer interface {
 }
 
 type LogProcess struct {
-	rc    chan string // 读取通道
+	rc    chan []byte // 读取通道
 	wc    chan string // 写入通道
 	read  Reader      //读取器
 	write Writer      //写入器
@@ -31,22 +35,51 @@ type WriteToInfluxDB struct {
 }
 
 //读取模块
-func (r *ReadFromFile) Read(rc chan string) {
-	line := "message"
-	rc <- line
+func (r *ReadFromFile) Read(rc chan []byte) {
+	//打开文件
+	f, err := os.Open(r.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file error: %s", err.Error()))
+	}
+
+	//从文件末尾开始逐行读取文件內容
+	//将文件指针移到末尾
+	f.Seek(0, 2)
+
+	rd := bufio.NewReader(f)
+
+	for {
+		line, err := rd.ReadBytes('\n')
+		//文件末尾，等待后重新循环
+		if err == io.EOF {
+			//暂停500ms,以免长时间的CPU占用
+			time.Sleep(500 * time.Microsecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("ReadBytes error: %s", err.Error()))
+		}
+		//去除行尾换行符
+		rc <- line[:len(line)-1]
+	}
+
 	wg.Done()
 }
 
 //写入模块
 func (w *WriteToInfluxDB) Write(wc chan string) {
-	fmt.Println(<-wc)
+	for v := range wc {
+		fmt.Println(v)
+	}
+
 	wg.Done()
 }
 
 // 解析模块
 func (l *LogProcess) Process() {
-	data := <-l.rc
-	l.wc <- strings.ToUpper(data)
+	for v := range l.rc {
+		l.wc <- strings.ToUpper(string(v))
+	}
+
 	wg.Done()
 }
 
@@ -54,7 +87,7 @@ var wg sync.WaitGroup
 
 func main() {
 	r := &ReadFromFile{
-		path: "/tmp/access.log",
+		path: "./access.log",
 	}
 
 	w := &WriteToInfluxDB{
@@ -62,7 +95,7 @@ func main() {
 	}
 
 	lp := &LogProcess{
-		rc:    make(chan string),
+		rc:    make(chan []byte),
 		wc:    make(chan string),
 		read:  r,
 		write: w,
