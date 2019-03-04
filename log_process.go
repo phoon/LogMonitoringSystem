9581e61cc93aa6d0f389330128197a9ba29945d2
Peven <iampeven@gmail.com>
2019-03-04 15:19:41 +0800

    lesson 2-2

--- log_process.go
+++ log_process.go
@@ -6,17 +6,40 @@ import (
 	"sync"
 )
 
-type LogProcess struct {
-	rc          chan string // 读取通道
-	wc          chan string // 写入通道
-	path        string      // 读取文件的路径
-	influxDBDsn string      // influx data source
+//使用接口机制，增强程序可扩展性
+type Reader interface {
+	Read(rc chan string)
 }
 
-// 读取模块
-func (l *LogProcess) ReadFromFile() {
+type Writer interface {
+	Write(wc chan string)
+}
+
+type LogProcess struct {
+	rc    chan string // 读取通道
+	wc    chan string // 写入通道
+	read  Reader      //读取器
+	write Writer      //写入器
+}
+
+type ReadFromFile struct {
+	path string // 读取文件的路径
+}
+
+type WriteToInfluxDB struct {
+	influxDBDsn string // influx data source
+}
+
+//读取模块
+func (r *ReadFromFile) Read(rc chan string) {
 	line := "message"
-	l.rc <- line
+	rc <- line
+	wg.Done()
+}
+
+//写入模块
+func (w *WriteToInfluxDB) Write(wc chan string) {
+	fmt.Println(<-wc)
 	wg.Done()
 }
 
@@ -27,25 +50,27 @@ func (l *LogProcess) Process() {
 	wg.Done()
 }
 
-// 写入模块
-func (l *LogProcess) WriteToInfluxDB() {
-	fmt.Println(<-l.wc)
-	wg.Done()
-}
-
 var wg sync.WaitGroup
 
 func main() {
-	lp := &LogProcess{
-		rc:          make(chan string),
-		wc:          make(chan string),
-		path:        "/tmp/access.log",
+	r := &ReadFromFile{
+		path: "/tmp/access.log",
+	}
+
+	w := &WriteToInfluxDB{
 		influxDBDsn: "username:password...",
 	}
 
+	lp := &LogProcess{
+		rc:    make(chan string),
+		wc:    make(chan string),
+		read:  r,
+		write: w,
+	}
+
 	wg.Add(3)
-	go lp.ReadFromFile()
+	go lp.read.Read(lp.rc)
 	go lp.Process()
-	go lp.WriteToInfluxDB()
+	go lp.write.Write(lp.wc)
 	wg.Wait()
 }
